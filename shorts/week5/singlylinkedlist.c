14551c6eb043b251ee34ed8837876a16a065dd18
Adrian Arumugam <apa@moogzy.net>
2017-02-11 04:45:56 +0000

    adding singly linked list

--- shorts/week5/singlylinkedlist.c
+++ shorts/week5/singlylinkedlist.c
@@ -5,13 +5,15 @@
 
 /*
  * Use struct to store int array and next node
+ *
+ * For singly linked list.
  */
-typedef struct node
+typedef struct sllnode
 {
     int number;
-    struct node *next;
+    struct sllnode *next;
 }
-node;
+sllnode;
 
 // TODO - document
 int main(void)
@@ -19,26 +21,26 @@ int main(void)
     
     int nums[] = {100, 750, 980, 2904, 2983, 8885};
     
-    node *head = malloc(sizeof(node));
+    sllnode *head = malloc(sizeof(sllnode));
     head->number = 0;
     head->next = NULL;
     
    for(int i = 0; i < (sizeof(nums) / 4); i++)
    {
-       node *new_node = malloc(sizeof(node));
+       sllnode *new_node = malloc(sizeof(sllnode));
        
        new_node->number = nums[i];
        new_node->next = head;
        head = new_node;
    }
    
-   node *trav = head;
+   sllnode *trav = head;
    
    while(trav != NULL)
    {
        printf("Cycling through linked list from head to tail:\n");
        printf("%i\n", trav->number);
-       node *temp = trav;
+       sllnode *temp = trav;
        printf("Setting free the malloc for %i\n", trav->number);
        trav = trav->next;
        free(temp);
